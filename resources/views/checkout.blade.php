39c32d74072c10481074af81a6b86e34b3cc08d1
berk <ps239891@summacollege.nl>
2024-02-29 12:44:45 +0100

    al het oude teruggezet

---
+++ resources/views/checkout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Checkout</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="">
+<header class="">
+    <nav>
+        <ul>
+            <li><a href="checkout">Chechout</a></li>
+            <li><a href="order">Order</a></li>
+            <li><a href="/">Homepage</a></li>
+        </ul>
+    </nav>
+</header>
+</body>
+</html>
