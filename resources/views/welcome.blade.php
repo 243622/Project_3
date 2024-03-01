bf153ce182f675d20ed51801451fb4d011ac1862
Thomas <Thomasvanelswijk@hotmail.com>
2024-03-01 10:06:23 +0100

    kleine update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,19 @@
+<style>
+    header{
+        background-color: #333;
+    }
+    body{
+        color: #fff;
+    }
+    h1{
+        color: black;
+    }
+    p{
+
+        color: #333;
+    }
+</style>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,34 +23,34 @@
     @vite('resources/css/app.css')
 </head>
 <body class="">
-<header class="flex border-4 border-amber-950 justify-around gap-3 items-center">
+<header class="flex justify-around gap-3 items-center">
     <nav>
         <ul class="flex flex-row space-x-4">
             <li><a class="text-xl" href="">Homepage</a></li>
             <li><a class="text-xl" href="order">Order</a></li>
-            <li><a class="text-xl" href="checkout">Chechout</a></li>
+            <li><a class="text-xl" href="checkout">Checkout</a></li>
             <a class="w-24 space-x-2"><img src="/PizzaFotos/_09253db4-5f28-4da4-b255-46c05fda052b-removebg-preview.png"></a>
         </ul>
     </nav>
 </header>
 
-<div class="grid grid-cols-2 text-center border-green-800 justify-items-start gap-10 p-12 bg-green-300">
-    <div class="justify-end">
+<div class="grid grid-cols-2 gap-32 p-12">
+    <div class="grid place-content-end text-center">
         <p><img class="h-52" src="/PizzaFotos/TonijnPizza.png"></p>
         <h1>Tonijn Pizza</h1>
-        <p>beschrijving m-28</p>
+        <button onclick=""></button>
     </div>
-    <div>
+    <div class="grid place-content-start text-center">
         <p><img class="h-52" src="/PizzaFotos/SalamiPizza.png"></p>
         <h1>Salami Pizza</h1>
         <p>beschrijving</p>
     </div>
-    <div>
-        <p><img class="h-52 w-56" src="/PizzaFotos/HawaiiPizza.png"></p>
+    <div class="grid place-content-end text-center">
+        <p><img class="h-52" src="/PizzaFotos/HawaiiPizza.png"></p>
         <h1>Hawaii Pizza</h1>
         <p>beschrijving</p>
     </div>
-    <div>
+    <div class="grid place-content-start text-center">
         <p><img class="h-52" src="/PizzaFotos/BarbecueChicken.png"></p>
         <h1>Barbeque Chicken Pizza</h1>
         <p>beschrijving</p>
