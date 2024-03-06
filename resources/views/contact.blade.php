f3b139eb3eea399c474fc845fa27eddfd3d51c4a
GoudVis <127117563+243622@users.noreply.github.com>
2024-03-06 09:53:49 +0100

    titel aangepast contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order Pizza</title>
+    <title>Contact</title>
     @vite(['resources/css/app.css','resources/css/custom.css', 'resources/js/app.js', 'resources/css/contact.css'])
 </head>
     <body>
