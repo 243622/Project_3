5dcde8e80e4f4f8b12cb74abf1ba77ed314de5a9
GoudVis <127117563+243622@users.noreply.github.com>
2024-03-07 08:44:09 +0100

    De migrations aangepast aan de bestaande tabel pizza en ook het ERD diagram daar op aangepast

--- database/migrations/2024_03_06_125519_create_orders_table.php
+++ database/migrations/2024_03_06_125519_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id('order_id');
             $table->foreignId('customer_id')->references('customer_id')->on('customers')->cascadeOnDelete();
-            $table->foreignId('pizza_id')->references('pizza_id')->on('pizzas')->cascadeOnDelete();
+            $table->foreignId('pizza_id')->references('pizza_id')->on('pizza')->cascadeOnDelete();
             $table->integer('total_price')->lenght(4);
             $table->string('message', 250)->nullable();
             $table->string('status_pizza', 40);
