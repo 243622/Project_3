af450ffa7d3cfbaaa95ad9b0e20bdef496415a23
GoudVis <127117563+243622@users.noreply.github.com>
2024-04-09 09:23:51 +0200

    Regel code in app.je verwijdert en nieuwe colom toegevoegd aan users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name',40);
             $table->string('email',40)->unique();
+            $table->string('usertype')->default('user');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password',40);
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
