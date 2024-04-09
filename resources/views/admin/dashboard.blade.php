60dfa68eac3e07a08f531174d7d18c02a741d50e
GoudVis <127117563+243622@users.noreply.github.com>
2024-04-09 10:15:52 +0200

    standaart layout voor dashboard van admin toegevoegd

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Admin</title>
-</head>
-    <body>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
 
-        ADMIN
+    </x-slot>
 
-    </body>
-</html>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("you're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
